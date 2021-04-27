9e602a2fb3c077a6c2918e7a4c4061b6729afabd
Gatis Jansons <bobsters@inbox.lv>
2021-04-27 12:04:37 +0100

    Updated Main Window. Added User login recording into database. Requires checking why not updating existing usernames with new data

--- Enginering Database/MainWindow.xaml.cs
+++ Enginering Database/MainWindow.xaml.cs
@@ -1,45 +1,33 @@
 ﻿using Engineering_Database;
 
 using System;
+using System.Data.OleDb;
 using System.Security.Principal;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Threading;
 
-
 //[assembly: log4net.Config.XmlConfigurator(Watch = true)]
 //[assembly: AssemblyVersion("2.10.*")]
 namespace Enginering_Database
 {
-
-
 	public partial class MainWindow : Window
 	{
+		private readonly string userName = WindowsIdentity.GetCurrent().Name;
+		private readonly UserSettings userSett = new UserSettings();
+		private UserSettings userMaintenSett = new UserSettings();
 
-
-		readonly string userName = WindowsIdentity.GetCurrent().Name;
-		readonly UserSettings userSett = new UserSettings();
-		UserSettings userMaintenSett = new UserSettings();
 		public MainWindow()
 		{
-
-
-
-
-
 			WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
-
-
 
 			DatabaseClass dtC = new DatabaseClass();
 
-
 			Application currApp = Application.Current;
 			currApp.ShutdownMode = ShutdownMode.OnLastWindowClose;
-
+			currApp.Exit += OnApplicationExit;
 
 			dtC.ConnectDB();
-
 
 			InitializeComponent();
 			OnStartupCheckMaintenance();
@@ -49,17 +37,12 @@ namespace Enginering_Database
 			string displayableVersion = $"{version}";
 			VersionData.Content = displayableVersion;
 
-
-
-
 			//userSett.openSettings();
-
 
 			DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
 			dispatcherTimer.Tick += new EventHandler(MaintenanceActivation);
 			dispatcherTimer.Interval = new TimeSpan(0, 0, 10);
 			dispatcherTimer.Start();
-
 
 			if (dtC.DBStatus() == "DB connected")
 			{
@@ -71,10 +54,6 @@ namespace Enginering_Database
 			}
 			fileExistsLabelData.Content = dtC.DBStatus();
 
-
-
-
-
 			AppDomain currentDomain = AppDomain.CurrentDomain;
 			currentDomain.UnhandledException += new UnhandledExceptionEventHandler(MyHandler);
 
@@ -83,30 +62,148 @@ namespace Enginering_Database
 			clock.Interval = new TimeSpan(0, 0, 1);
 			clock.Start();
 
-
-
+			RecordUserLogin();
 
 			dtC.CloseDB();
-
 		}
 
+		private void OnApplicationExit(object sender, EventArgs e)
+		{
+			UpdateUserLogin(userName, "Offline");
+		}
 
+		public void RecordUserLogin()
+		{
+			int status = UserLoginStatus(userName);
 
-		static void MyHandler(object sender, UnhandledExceptionEventArgs args)
+			switch (status)
+			{
+				case 1:
+					MessageBox.Show("User is currently Online");
+					break;
+
+				case -1:
+					CreateUserLogin(userName);
+					break;
+
+				case 0:
+					UpdateUserLogin(userName, "Online");
+
+					break;
+
+				default:
+					MessageBox.Show("Something not right");
+					break;
+			}
+		}
+
+		public void CreateUserLogin(string login)
+		{
+			try
+			{
+				string ConnectionString = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source = engineeringDatabase.accdb; Jet OLEDB:Database Password = test";
+				using (var con = new OleDbConnection(ConnectionString))
+				{
+					con.Open();
+
+					string sql = @"INSERT INTO UsersOnline (UserName,userStatus,LastLoginDate,LastLoginTime) Values
+						(@prm_UserName,@prm_UserStatus,@prm_LastLoginDate,@prm_LastLoginTime)";
+
+					using (OleDbCommand query = new OleDbCommand(sql, con))
+					{
+						query.Parameters.AddWithValue("@prm_UserName", login);
+						query.Parameters.AddWithValue("@prm_UserStatus", 1);
+						query.Parameters.AddWithValue("@prm_LastLoginDate", DateTime.Now.ToShortDateString());
+						query.Parameters.AddWithValue("@prm_LastLoginTime", DateTime.Now.ToShortTimeString());
+
+						query.ExecuteNonQuery();
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+		public void UpdateUserLogin(string login, string statusText)
+		{
+			try
+			{
+				string ConnectionString = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source = engineeringDatabase.accdb; Jet OLEDB:Database Password = test";
+				using (var con = new OleDbConnection(ConnectionString))
+				{
+					con.Open();
+
+					string sql = $"UPDATE UsersOnline SET userStatus = @prm_UserStatus WHERE UserName = @prm_UserName";
+
+					using (OleDbCommand query = new OleDbCommand(sql, con))
+					{
+						query.Parameters.AddWithValue("@prm_UserName", login);
+						switch (statusText)
+						{
+							case "Offline":
+								query.Parameters.AddWithValue("@prm_UserStatus", 0);
+								break;
+
+							case "Online":
+								query.Parameters.AddWithValue("@prm_UserStatus", 1);
+								break;
+						}
+
+						query.Parameters.AddWithValue("@prm_LastLoginDate", DateTime.Now.ToShortDateString());
+						query.Parameters.AddWithValue("@prm_LastLoginTime", DateTime.Now.ToShortTimeString());
+
+						query.ExecuteNonQuery();
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+		public int UserLoginStatus(string login)
+		{
+			string ConnectionString = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source = engineeringDatabase.accdb; Jet OLEDB:Database Password = test";
+			using (var con = new OleDbConnection(ConnectionString))
+			{
+				con.Open();
+
+				string sql = @"SELECT userStatus from UsersOnline WHERE UserName = @prm_username";
+
+				using (OleDbCommand query = new OleDbCommand(sql, con))
+				{
+					query.Parameters.AddWithValue("@prm_username", login);
+
+					using (var reader = query.ExecuteReader())
+					{
+						if (reader.Read())
+						{
+							return Convert.ToInt32(reader[0]);
+						}
+						else
+						{
+							return -1;
+						}
+					}
+				}
+			}
+		}
+
+		private static void MyHandler(object sender, UnhandledExceptionEventArgs args)
 		{
 			Exception e = (Exception)args.ExceptionObject;
 			UserErrorWindow userError = new UserErrorWindow();
 			userError.errorMessage = e.Message;
 			userError.CallWindow();
-
 		}
 
 		private void Button_Click(object sender, RoutedEventArgs e)
 		{
-
 			addData addnew = new addData();
 			addnew.ShowDialog();
-
 		}
 
 		private void Button_Click_1(object sender, RoutedEventArgs e)
@@ -116,7 +213,6 @@ namespace Enginering_Database
 			win2.ShowDialog();
 		}
 
-	
 		private void SettingsShow(object sender, RoutedEventArgs e)
 		{
 			//UserSettings userSett = new UserSettings();
@@ -124,41 +220,29 @@ namespace Enginering_Database
 
 			if (userName != userSett.SubAdmin1 || userName != userSett.SubAdmin2 || userName != userSett.UserName)
 			{
-
 				PasswordRequest("Settings");
 			}
 			else
 			{
-
-
 				userSett.ShowDialog();
 			}
 		}
 
-
-
-
-
 		public static void UpdateStat()
 		{
-
 		}
-
 
 		private void MenuItem_Click(object sender, RoutedEventArgs e)
 		{
-
 		}
 
 		private void PasswordRequest(string target)
 		{
-
 			passwordWindow passwordWindow = new passwordWindow();
 			passwordWindow.targetWindow = target;
 			passwordWindow.ShowDialog();
-
-
 		}
+
 		protected override void OnClosed(EventArgs e)
 		{
 			base.OnClosed(e);
@@ -167,8 +251,6 @@ namespace Enginering_Database
 
 		public string GetCPU()
 		{
-
-
 			string cpu = Environment.GetEnvironmentVariable("PROCESSOR_ARCHITECTURE");
 			if (cpu.IndexOf("64") > 0)
 			{
@@ -179,23 +261,16 @@ namespace Enginering_Database
 				cpu = "32 bit";
 			}
 			return cpu;
-
 		}
 
 		public void MaintenanceActivation(object sender, EventArgs e)
 		{
-
-
-
 			userMaintenSett.openSettings();
-
-
 
 			if (userMaintenSett.Maintenance == "Yes")
 			{
 				MaintenanceLabel.Visibility = Visibility.Visible;
 
-			
 				InsertDataButton.Visibility = Visibility.Hidden;
 				InsertDataImage.Visibility = Visibility.Hidden;
 				ViewDatabaseButton.Visibility = Visibility.Hidden;
@@ -203,24 +278,21 @@ namespace Enginering_Database
 				AdministratorButton.Visibility = Visibility.Hidden;
 				AdministratorImage.Visibility = Visibility.Hidden;
 				PasswordProtectImage2.Visibility = Visibility.Hidden;
-				
 
 				if (userName != userMaintenSett.UserName || userName != userMaintenSett.SubAdmin1 || userName != userMaintenSett.SubAdmin2)
 				{
-
 					DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
 					MaintenanceLabel2.Visibility = Visibility.Visible;
 					dispatcherTimer.Tick += new EventHandler(CloseApp);
 					dispatcherTimer.Interval = new TimeSpan(0, 0, 10);
 					dispatcherTimer.Start();
-
 				}
 			}
 			else
 			{
 				MaintenanceLabel.Visibility = Visibility.Hidden;
 				MaintenanceLabel2.Visibility = Visibility.Hidden;
-				
+
 				InsertDataButton.Visibility = Visibility.Visible;
 				InsertDataImage.Visibility = Visibility.Visible;
 				ViewDatabaseButton.Visibility = Visibility.Visible;
@@ -228,19 +300,9 @@ namespace Enginering_Database
 				AdministratorButton.Visibility = Visibility.Visible;
 				AdministratorImage.Visibility = Visibility.Visible;
 				PasswordProtectImage2.Visibility = Visibility.Visible;
-				
 			}
-
-
-
-
-
-
-
-
-
-
 		}
+
 		public void OnStartupCheckMaintenance()
 		{
 			//UserSettings userMaintenSett = new UserSettings();
@@ -250,7 +312,7 @@ namespace Enginering_Database
 			{
 				MaintenanceLabel.Visibility = Visibility.Visible;
 				//MaintenanceLabel2.Visibility = Visibility.Visible;
-				
+
 				InsertDataButton.Visibility = Visibility.Hidden;
 				InsertDataImage.Visibility = Visibility.Hidden;
 				ViewDatabaseButton.Visibility = Visibility.Hidden;
@@ -258,20 +320,15 @@ namespace Enginering_Database
 				AdministratorButton.Visibility = Visibility.Hidden;
 				AdministratorImage.Visibility = Visibility.Hidden;
 				PasswordProtectImage2.Visibility = Visibility.Hidden;
-				
 
 				if (userName != userMaintenSett.UserName || userName != userMaintenSett.SubAdmin1 || userName != userMaintenSett.SubAdmin2)
 				{
-
 					DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
 					MaintenanceLabel2.Visibility = Visibility.Visible;
 					dispatcherTimer.Tick += new EventHandler(CloseApp);
 					dispatcherTimer.Interval = new TimeSpan(0, 0, 10);
 					dispatcherTimer.Start();
-
 				}
-
-
 			}
 			else
 			{
@@ -283,11 +340,11 @@ namespace Enginering_Database
 				InsertDataImage.Visibility = Visibility.Visible;
 				ViewDatabaseButton.Visibility = Visibility.Visible;
 				ViewDatabaseImage.Visibility = Visibility.Visible;
-				
+
 				PasswordProtectImage2.Visibility = Visibility.Visible;
-				
 			}
 		}
+
 		public void CloseApp(object sender, EventArgs e)
 		{
 			this.Close();
@@ -306,13 +363,8 @@ namespace Enginering_Database
 			LocalTimeData.Content = d.ToString("HH:mm:ss");
 		}
 
-
-
-
 		private void AdministratorButton_Click(object sender, RoutedEventArgs e)
 		{
-			
-
 			userSett.openSettings();
 			if (userName == userSett.SubAdmin1 || userName == userSett.SubAdmin2 || userName == userSett.UserName)
 			{
@@ -320,20 +372,11 @@ namespace Enginering_Database
 				//lineMaintenance.Show();
 				Admin admin = new Admin();
 				admin.Show();
-
 			}
 			else
 			{
-
 				PasswordRequest("Admin");
-
 			}
-
 		}
 	}
 }
-
-
-
-
-
